e116fae403a3d9ea91930ccebb620bc4d7ed9942
amandal3 <aly@stevens.edu>
2019-10-17 15:49:25 -0400

    lines for elbow and wrist of left

--- index.js
+++ index.js
@@ -25,52 +25,82 @@
 // console.log(pose);
 
 var videoSize = 800;
-var minPartConfidence = 0.3;
 var capture;
 var keypoints = [];
 
+function setup() {
+    createCanvas(800, 600);
+    background(10, 0, 0);
+    i = 0;
+    minPartConfidence = 0.3;
+
+}
+video.addEventListener('loadeddata', function() {
+    this.currentTime = i;
+    console.log(i);
+});
+
+video.addEventListener('loadeddata', function() {
+    var duration = video.duration;
+    var i = 0;
+    var interval = setInterval(function() {
+        video.currentTime = i;
+        generateThumbnail(i);
+        i = i + 1;
+        if (i > duration) clearInterval(interval);
+    }, 200);
+});
+
+
+
+function generateThumbnail(i) {
+
+    var context = thecanvas.getContext('2d');
+    context.drawImage(video, 0, 0, 800, 600);
+    var dataURL = thecanvas.toDataURL();
+    var img = document.createElement('img');
+    img.setAttribute('src', dataURL);
+    document.getElementById('thecanvas').appendChild(img);
+    posenet.load().then(function(net) {
+        const pose = net.estimateSinglePose(img, {
+            flipHorizontal: true
+        });
+        return pose;
+    }).then(function(pose) {
+        console.log(pose);
+        keypoints = pose.keypoints;
+        draw();
+    })
+}
+
 function draw() {
-    capture = createCapture({
-        video: {
-          width: videoSize,
-          height: videoSize
-        }
-    });
-    capture.size(videoSize, videoSize);
-    capture.hide();
-
-    background(255);
-    image(capture, 0, 0, 800, 600);
-
-    noStroke();
-    // draw keypoints
     for (var i = 0; i < keypoints.length; i++) {
         var keypoint = keypoints[i];
-        // filter out keypoints that have a low confidence
         if (keypoint.score > minPartConfidence) {
-            // for wrists, make the part red
             if (i == posenet.partIds['leftWrist'] || i == posenet.partIds['rightWrist'])
+            //P5 only starts after you call setup()
+            //reason why we wrote window.onload = setup()
                 fill(255, 0, 0);
-            // all other parts are yellow
             else
-                fill(255, 255, 0);
+            //maybe instead of dots draw lines?
+            // fill(255, 255, 0);
 
-            ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+                ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
         }
+        //line(keypoint.postion.x[1], keypoint.position.y[1], keypoint.position.x[2], keypoint.position.y[2])
     }
+}
 
-    // get skeleton, filtering out parts wtihout
-    // a high enough confidence level
-    // if (keypoints.length > 0) {
-    //     stroke(255, 255, 0);
-    //     var skeleton = posenet.getAdjacentKeyPoints(keypoints, minPartConfidence);
-    //     for (var i = 0; i < skeleton.length; i++) {
-    //         // draw each line in the skeleton
-    //         var segment = skeleton[i];
-    //         line(
-    //             segment[0].position.x, segment[0].position.y,
-    //             segment[1].position.x, segment[1].position.y
-    //         );
-    //     }
-    // }
+video.addEventListener('seeked', function() {
+    generateThumbnail(i);
+    i += 0.5;
+    if (i <= this.duration) {
+        this.currentTime = i;
+    } else {
+        // Do something idk 
+    }
+});
+//onload call setup
+window.onload = setup;
+
 }
