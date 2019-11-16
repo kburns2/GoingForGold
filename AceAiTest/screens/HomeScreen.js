5e99844e3714a96c85df6782f2c533b52db70040
kburns2 <kburns2@stevens.edu>
2019-11-15 20:23:08 -0500

    updated home screen stylesheet

--- AceAiTest/screens/HomeScreen.js
+++ AceAiTest/screens/HomeScreen.js
@@ -1,49 +1,41 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-
+import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
+    <View style={styles.background}>
+      <ScrollView  style={styles.background}  contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
           <Image
             source={
               __DEV__
-                ? require('../assets/images/robot-dev.png')
+                ? require('../assets/images/tennisball.png')
                 : require('../assets/images/robot-prod.png')
             }
             style={styles.welcomeImage}
           />
         </View>
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Ace AI</Text>
+        <View style={styles.getStartedContainer}>       
+          <Text style={styles.title}> Ace AI </Text>
 
           <Text style={styles.getStartedText}>
-            Unlocking your potential through artifical intelligence
+           <Text> Unlocking your </Text>
+            <Text style={{fontWeight: "bold", color: '#AACC0D' }}>potential </Text>
+          <Text>through artifical intelligence </Text>
           </Text>
         </View>
 
         <View style={styles.loginContainer}>
-          <Text style={styles.getStartedText}>Username: </Text>
-          <Text style={styles.getStartedText}>Password: </Text>
+          <Text style={styles.userinfo}>Username: </Text>
+          <Text> </Text>
+          <Text style={styles.userinfo}>Password: </Text>
+          <Text> </Text>
         </View>
 
+        <DevelopmentModeNotice />
       </ScrollView>
     </View>
   );
@@ -57,42 +49,22 @@ function DevelopmentModeNotice() {
   if (__DEV__) {
     return (
       <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use
-        useful development tools.
+        Development mode is enabled
       </Text>
     );
   } else {
     return (
       <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
+        You are not in development mode
       </Text>
     );
   }
 }
 
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/development-mode/'
-  );
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
-  );
-}
-
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     backgroundColor: '#b2f5d9',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
   },
   contentContainer: {
     paddingTop: 30,
@@ -104,9 +76,14 @@ const styles = StyleSheet.create({
   },
   loginContainer: {
     alignItems: 'center',
-    marginTop: 10,
+    marginTop: 30,
     marginBottom: 20,
+    marginLeft: 40,
+    marginRight:40,
     backgroundColor: '#fff',
+    borderRadius:25,
+    paddingTop: 10,
+    paddingBottom:10,
   },
   welcomeImage: {
     width: 100,
@@ -118,6 +95,7 @@ const styles = StyleSheet.create({
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
+    marginVertical: 20,
   },
   homeScreenFilename: {
     marginVertical: 7,
@@ -130,11 +108,31 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     paddingHorizontal: 4,
   },
+  developmentModeText: {
+    marginTop: 150,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 72,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 72,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
   getStartedText: {
-    fontSize: 24,
+    fontSize: 19,
     color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
     textAlign: 'center',
+  },
+  userinfo: {
+    fontSize: 24,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'left',
   },
   tabBarInfoContainer: {
     position: 'absolute',
